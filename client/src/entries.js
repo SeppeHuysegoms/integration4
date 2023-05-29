35abdc02df959cc64a6e37c68fbb48a81dc5e6ef
SeppeHuysegoms <seppe.huysegoms@student.howest.be>
2023-05-29 15:06:29 +0200

    register

--- client/src/entries.js
+++ client/src/entries.js
@@ -1,3 +1,20 @@
 import localforage from "localforage";
 import { graphQLRequest } from "./utils/graphql.js";
 
+export async function register(name, email, password) {
+  console.log("registering", email, password, name);
+  const result = await graphQLRequest(
+    `mutation MyMutation {
+  register(
+    email: "${email}", password: "${password}" username: "${name}") {
+     jwt
+      user {
+        id
+      }
+  }
+}`
+  );
+
+  console.log("authenticate result", result);
+  return result.data.register;
+}
--- client/src/routes/Register.jsx
+++ client/src/routes/Register.jsx
@@ -1,30 +1,38 @@
 import { Form, redirect } from "react-router-dom";
-import { authenticate } from "../auth";
+import { register } from "../entries";
 
 export async function action({ request, params }) {
   const formData = await request.formData();
-  const { email, password } = Object.fromEntries(formData);
-
-  const { jwt, user } = await authenticate(email, password);
-
+  const { name,email, password } = Object.fromEntries(formData);
+console.log(email, password,name)
+  const { jwt, user } = await register(name, email, password);
+console.log(jwt, user)
   localStorage.setItem("jwt", jwt);
   localStorage.setItem("user", JSON.stringify(user));
-
   return redirect("/");
 }
 
 const Login = () => {
   return (
     <div style={{ padding: "2rem" }}>
-      <h1>Login</h1>
+      <h1>Register</h1>
       <Form method="post" id="contact-form">
+        <label className="flexColumn">
+
+          <span>Name</span>
+          <input
+            type="text"
+            name="name"
+            placeholder="Name"
+          />
+        </label>
+
         <label className="flexColumn">
           <span>Email</span>
           <input
             type="email"
             name="email"
             placeholder="Email"
-            defaultValue="seppe.huysegoms1@student.howest.be"
           />
         </label>
 
@@ -34,7 +42,6 @@ const Login = () => {
             type="password"
             name="password"
             placeholder="Password"
-            defaultValue="123456"
           />
         </label>
 
