79198f1259f54de13f37892ec4d544bfd1e7f0cb
SeppeHuysegoms <seppe.huysegoms@student.howest.be>
2023-06-07 22:36:38 +0200

    schrijf verhaal

---
+++ client/src/routes/SchrijfVerhaal.jsx
@@ -0,0 +1,41 @@
+import { Link } from "react-router-dom";
+import { useState } from "react";
+
+export default function Index() {
+  const [story, setStory] = useState();
+  let verhaal
+  /*if (localStorage.getItem("story")) {
+    setStory(localStorage.getItem("story"));
+    console.log(story);
+  }*/
+
+  return (
+    <>
+      <h1> Schrijf je verhaal </h1>;
+      <p>
+        Ideaal! Nu jouw plaats gekozen is, kan je een persoonlijke boodschap mee
+        sturen.
+      </p>
+      <p>Waarom deze plek? Wat maakt deze plek voor jou zo bijzonder?</p>
+      <textarea
+        name="name"
+        placeholder="Typ hier je verhaal"
+        //onChange={(e) => handleChange(e, setStory)}
+        //value={story}
+        rows="3"
+      />
+      <Link
+        onClick={() => {
+          localStorage.setItem("story", story);
+        }}
+        to="/schrijfverhaal"
+      >
+        Volgende
+      </Link>
+    </>
+  );
+}
+
+const handleChange = (event, setStory) => {
+  setStory(event.target.value);
+};
