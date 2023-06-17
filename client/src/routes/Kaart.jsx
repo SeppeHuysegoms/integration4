0f3de9aec0ae2e679308bce3879fb175bd683aff
SeppeHuysegoms <seppe.huysegoms@student.howest.be>
2023-06-17 20:26:33 +0200

    kaart css mobile

--- client/src/routes/Kaart.jsx
+++ client/src/routes/Kaart.jsx
@@ -5,8 +5,17 @@ import "../style/bloeiendePlekken.css";
 import icon from "../assets/flower.png";
 import arrow from "../assets/images/arrow.svg";
 import bloeiendePlekkenMobile from "../assets/images/bloeiendePlekkenMobile.png";
-import Lottie from "lottie-web";
+import Lottie from "lottie-react";
 import klok from "../assets/klok.json";
+import locatie from "../assets/locatie.json";
+import plant from "../assets/plant.json";
+import zoek from "../assets/images/zoek.svg";
+import marker from "../assets/marker.svg";
+import infoWindowZaadjes from "../assets/infoWindowZaadjes.svg";
+import jouwVerhaal from "../assets/images/jouwVerhaal.png";
+import instagramEmpty from "../assets/instagramEmpty.svg";
+import jouwVerhaalDesktop from "../assets/images/jouwVerhaalDesktop.png";
+import bloeiendePlekkenDesktop from "../assets/images/bloeiendePlekkenDesktop.png";
 
 import { createRoot } from "react-dom/client";
 import { getStories } from "../entries";
@@ -37,21 +46,7 @@ const App = () => {
     }, {})
   );
 
-  //lottie 
 
-  const container = useRef(null);
-
-  useEffect(() => {
-    Lottie.loadAnimation({
-      container: document.querySelector(".animatie"),
-      renderer: "svg",
-      loop: true,
-      autoplay: true,
-      animationData: klok,
-    });
-
-  
-  }, []);
 
   const [selectedStory, setSelectedStory] = useState();
   return (
@@ -68,12 +63,19 @@ const App = () => {
             </h1>
             <p className="uitroepteken--kaart">!</p>
           </div>
-
+          <picture className="header__image--kaart">
+            <source
+              media="(min-width: 800px)"
+              srcSet={bloeiendePlekkenDesktop}
+            />
+            <source media="(min-width: 300px)" srcSet={bloeiendePlekkenMobile} />
+            
           <img
             src={bloeiendePlekkenMobile}
             alt="skatepark kortrijk"
             className="header__image header__image--kaart"
           />
+          </picture>
         </div>
 
         <div className="header__content--kaart">
@@ -82,6 +84,7 @@ const App = () => {
             plaats ook b(l)oeit of ontdek misschien een nieuwe plek.
           </p>
           <a href="#map" className="button">
+            <img src={arrow} alt="arrow" className="arrowButton" />
             Bekijk kaart
           </a>
         </div>
@@ -90,15 +93,20 @@ const App = () => {
       <section className="kaartBeeld2">
         <ul className="kaartBeeld2__list">
           <li className="list__item--kaart">
-            <div className="animatie"></div>
+            <Lottie animationData={klok} className="item__animatie--klok" />
             <h2>{difference}</h2>
             <p>Dagen om deel te nemen</p>
           </li>
           <li className="list__item--kaart">
+            <Lottie animationData={plant} className="item__animatie--plant" />
             <h2>{stories.length}</h2>
             <p>Zaadjes geplant</p>
           </li>
           <li className="list__item--kaart">
+            <Lottie
+              animationData={locatie}
+              className="item__animatie--locatie"
+            />
             <h2>{locations.length}</h2>
             <p>Verschillende b(l)oeiende plekken</p>
           </li>
@@ -106,26 +114,48 @@ const App = () => {
       </section>
 
       <section className="section section--kaart">
-        <h2>Ontdek de kaart</h2>
         <MyMap stories={stories} id="map" />
       </section>
 
       <section className="section section--jouwVerhaal">
-        <h2>Wat is jouw verhaal?</h2>
-        <p>
-          Heb jij zelf ook een plek die je zou laten willen bloeien in Kortrijk?
-          Zoek onze posters in de stad of houd onze Instagram pagina in de
-          gaten. Je kan namelijk kans maken op een leuke extra!
-        </p>
-        <a>@Bloeiend!</a>
-        <p>
-          Te veel moeite voor je? Don’t worry! Je kan ook vanaf hier simpelweg
-          je verhaal planten.
-        </p>
-        <Link to="/plant" className="button">
-          <img src={arrow} alt="arrow" className="arrowButton" />
-          Plant jouw verhaal
-        </Link>
+        <div className="jouwVerhaal__titel">
+          <h2>
+            Wat is <br /> jouw verhaal
+            <span className="uitroepteken">?</span>{" "}
+          </h2>
+          <picture className="jouwVerhaal__image">
+            <source media="(min-width: 800px)" srcSet={jouwVerhaalDesktop} />
+            <source media="(min-width: 300px)" srcSet={jouwVerhaal} />
+
+            <img
+              className="jouwVerhaal__image"
+              src={jouwVerhaal}
+              alt="jouw verhaal"
+            />
+          </picture>
+        </div>
+        <div className="jouwVerhaal__content">
+          <p>
+            Heb jij zelf ook een plek die je zou laten willen bloeien in
+            Kortrijk? <br />
+            Zoek onze posters in de stad of houd onze Instagram pagina in de
+            gaten. Je kan namelijk kans maken op een leuke extra!
+          </p>
+          <a
+            className="jouwVerhaal__instagram"
+            href="https://instagram.com/bloeiendkortrijk?igshid=MzNlNGNkZWQ4Mg=="
+          >
+            <img src={instagramEmpty} alt="instagram logo" /> @BloeiendKortrijk
+          </a>
+          <p>
+            Te veel moeite voor je? Don’t worry! Je kan ook vanaf hier simpelweg
+            je verhaal planten.
+          </p>
+          <Link to="/plant" className="button">
+            <img src={arrow} alt="arrow" className="arrowButton" />
+            Plant jouw verhaal
+          </Link>
+        </div>
       </section>
     </>
   );
@@ -142,6 +172,8 @@ const mapOptions = {
 function MyMap({stories}) {
   console.log(stories);
   const [map, setMap] = useState();
+    const [input, setInput] = useState("");
+    const [voorstellen, setVoorstellen] = useState([]);
   const ref = useRef();
 
   useEffect(() => {
@@ -149,34 +181,135 @@ function MyMap({stories}) {
   }, []);
   console.log(ref);
   return (
-    <>
+    <div className="kaart">
+      <ZoekVeld
+        input={input}
+        setInput={setInput}
+        voorstellen={voorstellen}
+        setVoorstellen={setVoorstellen}
+        map={map}
+      />
       <div ref={ref} className="test" id="map" />
       {map && <Bloeiend map={map} stories={stories} />}
-    </>
+    </div>
   );
 }
 
-/*const InfoWindow = (selectedStory) => {
-  const [data, setData] = useState(selectedStory);
-  console.log(data);
-  if (!data) {
-    return (
-      <div>
-        <h2>{data[0].title}</h2>
-        <p>{data[0].placeId}</p>
-        <p>{data[0].position}</p>
+const ZoekVeld = ({
+  input,
+  setInput,
+  voorstellen,
+  setVoorstellen,
+  map,
+}) => {
+  const southWest = new google.maps.LatLng(
+    50.82040292260651,
+    3.2133969501072177
+  );
+  const northEast = new google.maps.LatLng(
+    50.83216251839117,
+    3.3122555150254587
+  );
+  const KortrijkBounds = new google.maps.LatLngBounds(southWest, northEast);
+  if (input) {
+    useEffect(() => {
+      const placesDemo = async () => {
+        const { AutocompleteService } = await google.maps.importLibrary(
+          "places"
+        );
+        const service = new AutocompleteService();
+        const prediction = await service.getPlacePredictions({
+          input: `${input}`,
+          CompositionRestrictions: { country: "be" },
+          locationRestriction: KortrijkBounds,
+          fields: ["place_id", "name", "address_components", "geometry "],
+        });
+        let arrayPrediction = [];
+        prediction.predictions.map((prediction) => {
+          arrayPrediction.push(prediction);
+        });
+        // console.log(arrayPrediction);
+        setVoorstellen(arrayPrediction);
+      };
 
-        <ul>
-          {data.map((story) => (
-            <li key={story.title}>{story.title}</li>
+      placesDemo();
+    }, [input]);
+  }
+
+  return (
+    <div className="zoekVeld--kaart">
+      <div className="zoekVeld__input">
+        <img src={zoek} alt="zoek" className="zoekVeld__zoek" />
+
+        <input
+          type="text"
+          placeholder="Zoek hier..."
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+      </div>
+      {voorstellen && input && (
+        <ul className="voorstellen__list">
+          {voorstellen.map((voorstel) => (
+            <li
+              className="voorstellen__item"
+              key={voorstel.place_id}
+              onClick={() =>
+                centerSearch(voorstel.place_id, map, setInput, voorstel)
+              }
+            >
+              <img src={marker} alt="marker" className="marker" />
+              <div>
+                <p>{voorstel.structured_formatting.main_text}</p>
+                <p>{voorstel.structured_formatting.secondary_text}</p>
+              </div>
+            </li>
           ))}
         </ul>
-      </div>
+      )}
+    </div>
+  );
+};
+
+const centerSearch = async (
+  placeID,
+  map,
+  setInput,
+  voorstel,
+  markersRef,
+  onSelectLocation
+) => {
+  setInput("");
+  let position = await getPos(placeID, map);
+
+  let location = new google.maps.LatLng(
+    position.geometry.location.lat(),
+    position.geometry.location.lng()
+  );
+
+  map.setCenter({
+    lat: location.lat(),
+    lng: location.lng(),
+  });
+  map.setZoom(16);
+
+};
+
+const getPos = async (placeID, map) => {
+  return new Promise(async (resolve) => {
+    const { PlacesService } = await google.maps.importLibrary("places");
+    const service = new PlacesService(map);
+    service.getDetails(
+      {
+        placeId: `${placeID}`,
+      },
+      (results) => {
+        resolve(results);
+      }
     );
-  } else {
-    return null;
-  }
-};*/
+  });
+};
+
 
 
 
@@ -201,11 +334,12 @@ const Bloeiend = ({ map, stories }) => {
       const { Marker } = await google.maps.importLibrary("marker");
       const { InfoWindow } = await google.maps.importLibrary("maps");
       const infoWindow = new InfoWindow();
+      infoWindow.className = "infoWindow2";
       const markers = data.map((data) => {
         let basis;
         let size;
         if (data.length < 25) {
-          basis = 9;
+          basis = 10;
           size = 0.75;
         } else if (data.length < 50) {
           basis = 15;
@@ -246,10 +380,30 @@ const Bloeiend = ({ map, stories }) => {
         });
         marker.setClass = "markerKaart";
         marker.addListener("click", () => {
+          map.setZoom(19);
            infoWindow.close();
-           infoWindow.setContent(`<div class="infoWindow"><h2>${marker.title}</h2> <ul>`+data.map((story) => ( `<li key=${story.title}>${story.verhaal}</li>`))+` 
-           </ul></div>`);
+           if(window.innerWidth < 800){
+           infoWindow.setOptions({ pixelOffset: new google.maps.Size(0, 450), anchor: marker });
+            } else {
+              infoWindow.setOptions({ pixelOffset: new google.maps.Size(-500, 300), anchor: marker });
+            }
+           console.log(data);
+           infoWindow.setContent(
+             `<div class="infoWindow"><h2>${marker.title}</h2> <div class="info__zaadjes"> <img class="infoWindowZaadjes" src="${infoWindowZaadjes}" alt="icon zaadjes"
+            className="header__image header__image--kaart" /> <p>${data.length} geplante zaadjes </p> </div> <h3> Uitgelichte verhalen voor deze plek </h3>  <ul>` +
+               data.map(
+                 (story) => `<li key=${story.id}>
+                  <p>${story.verhaal}</p>
+                  <p>${splitDate(story.dateCreated)} </p> </li>`
+               ) +
+               ` 
+           </ul></div>`
+           );
            infoWindow.open(marker.map, marker);
+           map.setCenter({
+             lat: position.lat(),
+             lng: position.lng(),
+           });
           console.log("click");
           console.log(marker.placeId);
         });
@@ -263,86 +417,13 @@ const Bloeiend = ({ map, stories }) => {
     setMarkers(groupByPlaceId);
   }, [stories]);
 };
-
-/*
-function Bloeiend({ map, setSelectedStory }) {
-
-  const groupByPlaceId = Object.values(
-    stories.reduce((group, story) => {
-      const { placeId } = story;
-      group[placeId] = group[placeId] ?? [];
-      group[placeId].push(story);
-      return group;
-    }, {})
-  );
-  
-  console.log(groupByPlaceId);
-  const [data, setData] = useState(groupByPlaceId);
-  const [hover, setHover] = useState();
-  const beachFlagImg = document.createElement("img");
-  beachFlagImg.src =
-    "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png";
-
-  return (
-    <>
-      {Object.entries(data).map(([key, data]) => (
-        <Marker
-          key={key}
-          map={map}
-          position={data[0].position}
-          size={data.length}
-
-        >
-          <div
-            className={`marker ${hover ? "hover" : ""}`}
-            // onMouseEnter={() => {
-            //   setHover(key);
-            //   console.log("click");
-            //   setSelectedStory(data);
-            //   console.log(data);
-            // }}
-          >
-            <h2>TESTEEEEE</h2>
-            <img
-              src="https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png"
-              alt="bachflag"
-              style={{ width: data.length * 10, height: data.length * 10 }}
-              onClick={() => {
-                console.log("click");
-              }}
-            />
-          </div>
-        </Marker>
-      ))}
-    </>
-  );
+function splitDate(date) {
+  const split = date.split("T");
+  const splitDate = split[0];
+  const splitDate2 = splitDate.split("-");
+  const splitDate3 = splitDate2[2] + "/" + splitDate2[1] + "/" + splitDate2[0];
+  return splitDate3;
 }
 
-const Marker = ({ map, position, children, size }) => {
-  const markerRef = useRef();
-  const rootRef = useRef();
-
-
-  useEffect(() => {
-    if (!rootRef.current) {
-      const container = document.createElement("div");
-
-      container.className = "marker2";
-      container.style.color = "red";
-      rootRef.current = createRoot(container);
-      markerRef.current = new google.maps.marker.AdvancedMarkerView({
-        position,
-        content: container,
-      });
-    }
-  }, []);
-
-  useEffect(() => {
-    rootRef.current.render(children);
-    markerRef.current.size = size;
-    markerRef.current.position = position;
-    markerRef.current.map = map;
-  }, [map, position, children, size]);
-};*/
 
 export default App;
